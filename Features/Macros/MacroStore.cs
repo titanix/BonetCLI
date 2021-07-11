4a79ac2b99a9d3403ae9b7033478c46d35e414ea
Louis Lecailliez <louis.lecailliez@outlook.fr>
2021-07-11 19:51:59 +0900

    Adding two default macros.

--- Features/Macros/MacroStore.cs
+++ Features/Macros/MacroStore.cs
@@ -11,6 +11,8 @@ namespace BonetIDE
         {
             macros.Add("mm 2 1 ; a 3 ; d 3");
             macros.Add("mm 2 4 1 3 ; a 5 ; d 3 4 5");
+            macros.Add("mm 4 2 3 1 ; a 5 ; d 3 4 5");
+            macros.Add("mm 2 3 1 4 ; a 5 ; d 3 4 5");
         }
         
         public void AddMacro(string macro)
