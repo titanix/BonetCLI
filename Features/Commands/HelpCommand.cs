599e02b46c8fa80f1e21ba0ed8b5cdb744c540a2
Louis Lecailliez <louis.lecailliez@outlook.fr>
2021-07-22 01:27:05 +0900

    Adding sorted result to [h]elp command output.

--- Features/Commands/HelpCommand.cs
+++ Features/Commands/HelpCommand.cs
@@ -11,8 +11,10 @@ namespace BonetIDE
         public ICommandResult Execute(IContext context, List<ICommandArgument> arguments)
         {
             Console.WriteLine("** Available commands **");
+
+            IEnumerable<ICommand> sorted = context.commands.OrderBy(a => a.GetType().Name);
             
-            foreach (ICommand command in context.commands)
+            foreach (ICommand command in sorted)
             {
                 string fullName = command.GetType().Name;
                 string shortName = fullName.Substring(0, fullName.IndexOf("Command"));
